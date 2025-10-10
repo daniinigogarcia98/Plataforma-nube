e6d69db06ca584443b7643c6c93229d7f189b566
Daniel iñigo garcia <daniinigogarcia98@gmail.com>
2025-10-10 11:14:25 +0200

    cambios

--- S3_SDK/S3.php
+++ S3_SDK/S3.php
@@ -1,11 +1,21 @@
 <?php
-require_once 'vendor/autoload.php';
-class S3 {
-    private $region='us-east-1';
 
-    public function __construct() {
-        // Conectarse con S3
+use Aws\S3\S3Client;
 
+class S3
+{
+    private $region = 'us-east-1';
+
+    public function __construct()
+    {
+        try {
+            // Conectarse con S3
+            $conexion = new S3Client([
+                'version' => 'latest',
+                'region' => $this->region,
+            ]);
+        } catch (\Throwable $th) {
+            echo '<h3 style="color:red;">'.$th->getMessage().'</h3>';
+        }
     }
 }
-?>
