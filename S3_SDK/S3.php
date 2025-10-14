3127c3ba5f0899fa0156ed3af4739eaf02227094
Daniel iñigo garcia <daniinigogarcia98@gmail.com>
2025-10-14 09:26:12 +0200

    Refactor S3 y controlador para mejorar la legibilidad y la estructura del código

--- S3_SDK/S3.php
+++ S3_SDK/S3.php
@@ -1,39 +1,62 @@
 <?php
-    require_once 'vendor/autoload.php';
+require_once 'vendor/autoload.php';
 
-    use Aws\S3\S3Client;
+use Aws\S3\S3Client;
 
-    class S3{
-        private $region = 'us-east-1';
-        private $conexion;
+class S3
+{
+    private $region = 'us-east-1';
+    private $conexion;
 
-        public function __construct(){
-           try {
-             //Conectar con S3
+    public function __construct()
+    {
+        try {
+            //Conectar con S3
             $this->conexion = new S3Client([
                 'version' => 'latest',
                 'region' => $this->region
             ]);
-             echo '<h3 style="color:green;">Conexión establecida</h3>';
-           } catch (\Throwable $th) {
-                global $error;
-                $error=$th->getMessage();
-           }
-        }
-
-        public function crearBucket($nombre){
-            $resultado = false;
-            try {
-                $this->conexion->createBucket([
-                    'Bucket' => $nombre
-                ]);
-                $resultado = true;
-            } catch (\Throwable $th) {
-               global  $error;
-               $error=$th->getMessage();
-            }
-
-            return $resultado;
+            //comprobar que conecta
+            //echo '<h3 style="color:green;">Conexión establecida</h3>';
+        } catch (\Throwable $th) {
+            global $error;
+            $error = $th->getMessage();
         }
     }
-?>
+
+    public function crearBucket($nombre)
+    {
+        $resultado = false;
+        try {
+            $this->conexion->createBucket([
+                'Bucket' => $nombre
+            ]);
+            $resultado = true;
+        } catch (\Throwable $th) {
+            global  $error;
+            $error = $th->getMessage();
+        }
+
+        return $resultado;
+    }
+
+    public function obtenerBuckets()
+    {
+        $resultado = array();
+        try {
+            //Obtenemis toda la informacion de los buckets 
+            //creado en S3
+            $tmp = $this->conexion->listBuckets();
+            //Devolvemos solamente el nombre de los Buckets
+            //Los Buckets esta la clave Bucket del array $tmp
+            foreach ($tmp['Buckets'] as $b) {
+                //El nombre está la clave Name del array $b
+                $resultado[] = $b['Name'];
+            }
+        } catch (\Throwable $th) {
+            global  $error;
+            $error = $th->getMessage();
+        }
+        return $resultado;
+    }
+}
--- S3_SDK/controlador.php
+++ S3_SDK/controlador.php
@@ -1,23 +1,26 @@
 <?php
 require_once 'S3.php';
 
-if(sizeof($_POST)>0){
-    $awsS3 = new S3();
-}
 
-if(isset($_POST['crearB'])){
-    if(empty($_POST['nombre'])){
+$awsS3 = new S3();
+
+//Recuperar los buckets para mostrar en los selects
+
+if ($awsS3 !=null) {
+   $buckets = $awsS3->obtenerBuckets();
+
+
+if (isset($_POST['crearB'])) {
+    if (empty($_POST['nombre'])) {
         $error = 'El nombre del bucket no puede estar vacío';
-    }
-    else{
-         if($awsS3->crearBucket($_POST['nombre'])){
+    } else {
+        if ($awsS3->crearBucket($_POST['nombre'])) {
             $mensaje = 'Bucket Creado';
-         }
-         else{
-            $error = 'Error al crear el bucket:'.$error;  
-         }
+        } else {
+            $error = 'Error al crear el bucket:' . $error;
+        }
     }
-   
 }
-
-?>
+}else {
+    $error='<h3 style="color:red;">No se puede Conectar con S3</h3>';
+}
