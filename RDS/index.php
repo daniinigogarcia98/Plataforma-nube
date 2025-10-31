31221d915b5216a53a0edc633a3bc623b1a7b7c8
Daniel iñigo garcia <daniinigogarcia98@gmail.com>
2025-10-31 11:16:59 +0100

    Añadir archivos para la gestión de tareas: crear base de datos, tablas y formulario HTML.

---
+++ RDS/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tareas</title>
+</head>
+<body>
+    <h1>Gestion de Tareas</h1>
+    <form action="" method="post">
+        <label>Título <input type="text" name="titulo" placeholder="Título"></label><br><br>
+        <label>Descripción <input type="text" name="descripcion" placeholder="Descripción"></label><br><br>
+        <label>Prioridad <select name="prioridad">
+            <option>Baja</option>
+            <option>Media</option>
+            <option>Alta</option>
+        </select></label><br><br>
+         <label>Estado <select name="estado">
+            <option>Pendiente</option>
+            <option>En Proceso</option>
+            <option>Completada</option>
+        </select></label><br><br>
+    </form>
+</body>
+</html>
