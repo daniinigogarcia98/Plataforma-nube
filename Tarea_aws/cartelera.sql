48d324ed253564bb454b951d95d439d3dc1b508c
Daniel Iñigo Garcia <daniinigogarcia98@gmail.com>
2025-11-26 14:28:06 +0100

    bd

--- Tarea_aws/cartelera.sql
+++ Tarea_aws/cartelera.sql
@@ -6,8 +6,13 @@ create table peliculas(
 	id_pelicula int auto_increment primary key,
     titulo varchar(100) not null,
 	director varchar(100) not null,
+    actor varchar(100) not null,
 	S3Fotos varchar(255),
     año varchar(100) not null,
     genero varchar(100) not null,
-    formato varchar(100) not null
+    formato enum('Streaming','Digital','DVD') not null default 'Streaming'
 )engine innodb;
+
+insert into peliculas values(
+default,'Bad Boys: Ride or Die',' Adil El Arbi, Bilall Fallah','Will Smith',null,'2024','Acción/Comedia','Streaming'
+);
